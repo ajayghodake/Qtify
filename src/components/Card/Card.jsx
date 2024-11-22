f835fbed966c44c9e9591a5ef65f647746039764
ajayghodake <it.ajaghodake@gmail.com>
2024-11-22 22:19:02 +0530

    card.jsx updated

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -28,8 +28,8 @@ export default function ActionAreaCard() {
 
   const responsive = {
     superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 10 },
-    desktop: { breakpoint: { max: 3000, min: 1024 }, items: 7 },
-    tablet: { breakpoint: { max: 1024, min: 464 }, items: 3 },
+    desktop: { breakpoint: { max: 3000, min: 1024 }, items: 6 },
+    tablet: { breakpoint: { max: 1024, min: 464 }, items: 4 },
     mobile: { breakpoint: { max: 464, min: 0 }, items: 2 },
   };
 
@@ -49,13 +49,15 @@ export default function ActionAreaCard() {
       <div className={`wrapper ${isExpanded ? "grid-layout" : ""}`}>
         {isExpanded ? (
           // Grid layout when expanded
-          apiData.map((album, index) => (
+          apiData?.map((album, index) => (
+            <div className="main">
             <div key={album.id || index} className="card-container">
               <div className="card-box">
                 <img src={album.image} alt={album.title} />
                 <Chip id="chip" label={`${album.follows} follows`} />
               </div>
               <div id="slug">{album.slug}</div>
+            </div>
             </div>
           ))
         ) : (
@@ -68,7 +70,7 @@ export default function ActionAreaCard() {
             renderButtonGroupOutside={false}
             arrows={true}
           >
-            {apiData.map((album, index) => (
+            {apiData?.map((album, index) => (
               <div key={album.id || index} className="card-container">
                 <div className="card-box">
                   <img src={album.image} alt={album.title} />
