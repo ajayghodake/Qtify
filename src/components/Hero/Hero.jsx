1b319af640571cbb6cf62fb49fac7029be20db5a
ajayghodake <it.ajaghodake@gmail.com>
2024-11-22 21:18:34 +0530

    Hero section is updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className="hero-container">
       <div className="hero-text">
-        <h1>100 Thousand songs, add-Free</h1>
+        <h1>100 Thousand Songs, ad-free</h1>
         <h1>Over thousand podcast episodes</h1>
       </div>
       <div className="hero-Img">
