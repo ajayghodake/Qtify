b1d5e6615f28e13297cbf11ca841043d2b810c5a
ajayghodake <it.ajaghodake@gmail.com>
2024-11-24 04:13:06 +0530

    Show more is now Show All

--- src/components/AlbumSection/albumSection.jsx
+++ src/components/AlbumSection/albumSection.jsx
@@ -39,7 +39,7 @@ export default function AlbumSection({endpoint, title}) {
       <div className="header">
         <h3>{title}</h3>
         <button className="toggle-button" onClick={toggleExpanded}>
-          {isExpanded ? "Collapse" : "Show More"}
+          {isExpanded ? "Collapse" : "Show All"}
         </button>
       </div>
 
